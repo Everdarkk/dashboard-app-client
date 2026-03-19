b37dcca635d8cf4ca0a9fb922fcbd50b9a19cdf9
Oleksandr Dorokhov <alex.dorokhov.mail@gmail.com>
2026-03-19 15:37:16 +0200

    Implemented Cloudinary image upload integration

---
+++ src/components/upload-widget.tsx
@@ -0,0 +1,103 @@
+import { CLOUDINARY_CLOUD_NAME, CLOUDINARY_UPLOAD_PRESET } from "@/constants"
+import { UploadWidgetValue } from "@/types"
+import { UploadCloudIcon } from "lucide-react"
+import { useEffect, useRef, useState } from "react"
+
+const UploadWidget = ({ value = null, onChange, disabled = false}) => {
+  const widgetRef = useRef<CloudinaryWidget | null>(null)
+  const onChangeRef = useRef(onChange)
+
+  const [preview, setPreview] = useState(null as UploadWidgetValue | null)
+  const [deleteToken, setDeleteToken] = useState<string | null>(null)
+  const [isRemoving, setIsRemoving] = useState(false)
+
+  const openWidget = () => {
+    if(!disabled) widgetRef.current?.open()
+  }
+
+  const removeFromCloudinary = async () => {
+
+  }
+
+  useEffect(() => {
+    setPreview(value)
+    if(!value) setDeleteToken(null)
+  }, [value])
+
+  useEffect(() => {
+    onChangeRef.current = onChange
+  }, [onChange])
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return
+
+    const initializeWidget = () => {
+      if(!window.cloudinary || widgetRef.current) return false
+
+      widgetRef.current = window.cloudinary.createUploadWidget({
+        cloudName: CLOUDINARY_CLOUD_NAME,
+        uploadPreset: CLOUDINARY_UPLOAD_PRESET,
+        multiple: false,
+        maxFileSize: 5 * 1024 * 1024, // 5MB
+        clientAllowedFormats: ['png', 'jpg', 'jpeg', 'webp'],
+      }, (error, result) => {
+        if (!error && result.event === "success") {
+          const payload: UploadWidgetValue = {
+            url: result.info.secure_url,
+            publicId: result.info.public_id,
+          }
+
+          // setPreview(payload)
+
+          setDeleteToken(result.info.delete_token ?? null)
+
+          onChangeRef.current?.(payload)
+        }
+      })
+
+      return true
+    }
+
+    if (initializeWidget()) return
+
+    const intervalId = window.setInterval(() => {
+      if (initializeWidget()) {
+        window.clearInterval(intervalId)
+      }
+    }, 500)
+
+    return () => window.clearInterval(intervalId)
+  },[])
+
+  return (
+    <div className='space-y-2'>
+      {preview ? (
+        <div className="upload-preview">
+          <img src={preview.url} alt="Uploaded file" />
+        </div>)
+      : <div 
+          className="upload-dropzone" 
+          role="button" 
+          tabIndex={0} 
+          onClick={openWidget} 
+          onKeyDown={(e) => {
+            if(e.key === 'Enter') {
+              e.preventDefault()
+            }
+            openWidget()
+          }}
+        >
+          <div className="upload-prompt">
+            <UploadCloudIcon className="icon" />
+            
+            <div>
+              <p>Click to upload photo</p>
+              <p>PNG, JPEG up to 5MB </p>
+            </div>
+          </div>  
+        </div>}
+    </div>
+  )
+}
+
+export default UploadWidget
